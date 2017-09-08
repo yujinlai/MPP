d71490bda5332d590c8554802be5bd7f7cf435c0
yujin <yujinlai@mum.edu>
2017-09-08 10:48:04 -0500

    AA

--- lab3/lesson3/labs/prob4/Property.java
+++
@@ -1,8 +0,0 @@
-package lesson3.labs.prob4;
-
-public abstract class Property {
-	public double computeRent() {
-		return 0.0;
-	};
-}
-
--- lab3/lesson3/labs/prob4/Trailer.java
+++
@@ -1,9 +0,0 @@
-package lesson3.labs.prob4;
-
-public class Trailer extends Property {
-
-	@Override
-	public double computeRent(){
-		return 500;
-	}
-}
