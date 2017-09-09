589f5861739053ddfc8924b5c9404373a2c2947e
zhoulangbian <joe.yuzhou2015@gmail.com>
2017-09-08 19:43:03 -0500

    lesson5 prob1 comitted by yuzhou

--- lesson5/labs/prob1/rulesets/BookRuleSet.java
+++ lesson5/labs/prob1/rulesets/BookRuleSet.java
@@ -68,7 +68,7 @@ public class BookRuleSet implements RuleSet {
 		} catch(NumberFormatException e) {
 			throw new RuleException("Price must be floating point number");
 		}
-		if(val.indexOf(val.length()-2) != '.')
+		if(val.charAt(val.length()-2-1 ) != '.')
 			throw new RuleException("Price must be a floating point number with two decimal places");
 		if((price-0.49) <0 || Math.abs(price - 0.49) <= 0.00000001) 
 			throw new RuleException("Price must be greater than 0.49");
--- lesson5/labs/prob1/rulesets/CDRuleSet.java
+++ lesson5/labs/prob1/rulesets/CDRuleSet.java
@@ -39,7 +39,7 @@ public class CDRuleSet implements RuleSet {
 		} catch(NumberFormatException e) {
 			throw new RuleException("Price must be floating point number");
 		}
-		if(val.indexOf(val.length()-2) != '.')
+		if(val.charAt(val.length()-2-1) != '.')
 			throw new RuleException("Price must be a floating point number with two decimal places");
 		if((price-0.49) <0 || Math.abs(price - 0.49) <= 0.00000000001) 
 			throw new RuleException("Price must be greater than 0.49");
