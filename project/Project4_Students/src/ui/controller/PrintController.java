f0edb62d5e629373d7ba5baeba549cc7f3be9773
zhoulangbian <joe.yuzhou2015@gmail.com>
2017-09-17 17:19:25 -0500

    modify print by YuZhou

---
+++ project/Project4_Students/src/ui/controller/PrintController.java
@@ -0,0 +1,64 @@
+package ui.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+
+import business.CheckOutRecord;
+import business.CheckoutRecordEntry;
+import business.ControllerInterface;
+import business.SystemController;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import ui.ruleengine.RuleException;
+import ui.ruleengine.RuleSet;
+import ui.ruleengine.RuleSetFactory;
+
+public class PrintController implements LibController {
+	@FXML
+	private TextField memberID;
+	
+	public TextField getMemberID() {
+		return memberID;
+	}
+
+	public void setMemberID(TextField memberID) {
+		this.memberID = memberID;
+	}
+	
+	@FXML
+	private Text actiontarget;
+
+	@FXML
+	protected void printRecord() {
+		try {			
+			RuleSet rules = RuleSetFactory.getRuleSet(PrintController.this);
+			rules.applyRules(PrintController.this);
+			ControllerInterface c = new SystemController();
+			CheckOutRecord record = c.getCheckoutRecord(memberID.getText().trim());
+			printConsole(record);
+		} catch (RuleException e1) {
+			actiontarget.setText(e1.getMessage());
+		} 
+	}
+	
+	private void printConsole(CheckOutRecord record) {
+		if(record == null) {
+			System.out.println("The libray member " + memberID.getText() +" doesn't have any checkout record.");
+			return;
+		}
+		ArrayList<CheckoutRecordEntry> entries = record.getCheckOutRecords();
+		String member = record.getMemberID();
+	    System.out.println("The libray member " + member +"'checkout records are as below:");
+	    System.out.format("%25s%25s%25s%25s",
+	    	    "Title", "Copy Number", "Checkout Date", "Due Date");
+	    System.out.println("");
+	    System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+	    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		for(CheckoutRecordEntry e : entries) {
+			System.out.format ("%25s%25s%25s%25s", e.getRequestedBook().getBook().getTitle(),
+		      e.getRequestedBook().getCopyNum(), sdf.format(e.getCheckoutDate()), sdf.format(e.getDueDate()));
+			System.out.println("");
+		}
+	}
+}
