5981a35b25c928956401c171f44350db8bd0a5cb
zhoulangbian <joe.yuzhou2015@gmail.com>
2017-09-17 23:50:10 -0500

    simplify the rule engine

--- project/Project4_Students/src/ui/ruleengine/RuleSetFactory.java
+++ project/Project4_Students/src/ui/ruleengine/RuleSetFactory.java
@@ -2,10 +2,6 @@ package ui.ruleengine;
 
 import java.util.HashMap;
 
-import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
-import ui.AddLibMemberWindow;
-import ui.LoginWindow;
 import ui.controller.AddBookCopyController;
 import ui.controller.AddMemberController;
 import ui.controller.CheckOverDueController;
@@ -16,43 +12,22 @@ import ui.controller.PrintController;
 
 final public class RuleSetFactory {
 	private RuleSetFactory(){}
-	static HashMap<Class<? extends Pane>, RuleSet> map1 = new HashMap<>();
-	static HashMap<Class<? extends Stage>, RuleSet> map2 = new HashMap<>();
-	static HashMap<Class<? extends LibController>, RuleSet> map3 = new HashMap<>();
+	static HashMap<Class<? extends LibController>, RuleSet> map = new HashMap<>();
 	static {
-		map1.put(AddLibMemberWindow.class, new LibraryMemberRuleSet());
-		map2.put(LoginWindow.class, new LoginRuleSet());
-		map3.put(LoginController.class, new LoginRuleSet());
-		map3.put(AddMemberController.class, new LibraryMemberRuleSet());
-		map3.put(CheckoutBookController.class, new CheckoutBookRuleSet());
-		map3.put(AddBookCopyController.class, new AddBookCopyRuleSet());
-		map3.put(PrintController.class, new PrintRecordsRuleSet());
-		map3.put(CheckOverDueController.class, new CheckOverDueRuleSet());
-	}
-	public static RuleSet getRuleSet(Pane c) {
-		Class<? extends Pane> cl = c.getClass();
-		if(!map1.containsKey(cl)) {
-			throw new IllegalArgumentException(
-					"No RuleSet found for this Component");
-		}
-		return map1.get(cl);
-	}
-	
-	public static RuleSet getRuleSet(Stage c) {
-		Class<? extends Stage> cl = c.getClass();
-		if(!map2.containsKey(cl)) {
-			throw new IllegalArgumentException(
-					"No RuleSet found for this Component");
-		}
-		return map2.get(cl);
+		map.put(LoginController.class, new LoginRuleSet());
+		map.put(AddMemberController.class, new LibraryMemberRuleSet());
+		map.put(CheckoutBookController.class, new CheckoutBookRuleSet());
+		map.put(AddBookCopyController.class, new AddBookCopyRuleSet());
+		map.put(PrintController.class, new PrintRecordsRuleSet());
+		map.put(CheckOverDueController.class, new CheckOverDueRuleSet());
 	}
 	
 	public static RuleSet getRuleSet(LibController c) {
 		Class<? extends LibController> cl = c.getClass();
-		if(!map3.containsKey(cl)) {
+		if(!map.containsKey(cl)) {
 			throw new IllegalArgumentException(
 					"No RuleSet found for this Component");
 		}
-		return map3.get(cl);
+		return map.get(cl);
 	}
 }
