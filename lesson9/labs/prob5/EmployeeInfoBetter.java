410fe985222bc01243e682c76c6d66cca7bbaba8
zhoulangbian <joe.yuzhou2015@gmail.com>
2017-09-20 23:27:51 -0500

    update prob 5 by Yu Zhou

--- lesson9/labs/prob5/EmployeeInfoBetter.java
+++ lesson9/labs/prob5/EmployeeInfoBetter.java
@@ -1,8 +1,10 @@
 package lesson9.labs.prob5;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
 import java.util.function.*;
 
@@ -10,16 +12,20 @@ public class EmployeeInfoBetter {
 	static enum SortMethod {BYNAME, BYSALARY};
 	Function<Employee, String> byName = e -> e.getName();
 	Function<Employee, Integer> bySalary = e -> -e.getSalary();
+	HashMap<SortMethod, List<Function>> functionMap = 
+			new HashMap<SortMethod, List<Function>>() {
+		{
+			put(SortMethod.BYNAME, Arrays.asList(byName,bySalary));
+			put(SortMethod.BYSALARY, Arrays.asList(bySalary, byName));
+		}
+	};
 	
+	@SuppressWarnings("unchecked")
 	public void sort(List<Employee> emps, final SortMethod method) {
-		if(method == SortMethod.BYNAME) {
-			Collections.sort(emps, Comparator.comparing(byName).thenComparing(bySalary));
-		} else {
-			Collections.sort(emps, Comparator.comparing(bySalary).thenComparing(byName));
-		}			
-	}
-	
-	
+		Collections.sort(emps,
+				Comparator.comparing(functionMap.get(method).get(0)).
+				     thenComparing(functionMap.get(method).get(1)));
+	}	
 	
 	public static void main(String[] args) {
 		List<Employee> emps = new ArrayList<>();
