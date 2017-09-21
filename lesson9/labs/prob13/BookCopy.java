263d7e5d3393623bf5b048bc92632f863687c80c
yujinlai <yujinlai@mum.edu>
2017-09-21 16:37:37 -0700

    added prob13

---
+++ lesson9/labs/prob13/BookCopy.java
@@ -0,0 +1,23 @@
+package lesson9.labs.prob13;
+
+@SuppressWarnings("unused")
+public class BookCopy {
+	private Book book;
+	private int copyNum;
+	private boolean isAvailable;
+	public BookCopy(Book book, int copyNum, boolean isAvailable) {
+		this.book = book;
+		this.copyNum = copyNum;
+		this.isAvailable = isAvailable;
+	}
+	public boolean isAvailable() {
+		return isAvailable;
+	}
+	
+	public void setAvailable(boolean available) {
+		isAvailable = available;
+	}
+	public void changeAvailability() {
+		isAvailable = !isAvailable;
+	}
+}
---
+++ lesson9/labs/prob13/Main.java
@@ -0,0 +1,19 @@
+package lesson9.labs.prob13;
+
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//setup
+		Book book = new Book("test", 3);
+		List<BookCopy> copies = book.getCopies();
+		copies.forEach(copy -> copy.changeAvailability());
+		
+		//test
+		System.out.println(book.isAvailable());
+	}
+
+}
